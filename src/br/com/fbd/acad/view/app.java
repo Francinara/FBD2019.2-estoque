17e1944f894d9aa1cdce8d4b406733d4abc6f07e
narin <narin@LAPTOP-4SQI8KKK>
2019-10-13 19:13:20 -0300

    projeto

--- src/br/com/fbd/acad/view/app.java
+++ src/br/com/fbd/acad/view/app.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -16,17 +17,19 @@ public class app extends Application {
 		
 		try {
 			Pane root;
-			root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+			root = FXMLLoader.load(getClass().getResource("fornecedor.fxml"));
 			Scene scene = new Scene(root, 1180, 680);
 			scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(false);
+			primaryStage.setTitle("Estoque");
+			Image anotherIcon = new Image(getClass().getResourceAsStream("images/venda.png"));
+            primaryStage.getIcons().add(anotherIcon);
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
-
 	}
 
 	public static void main(String[] args) {
