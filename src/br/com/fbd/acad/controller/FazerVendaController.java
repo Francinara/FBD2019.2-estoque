c884b254623d4fbfa4a55f0d8df43b7014e3151f
Francinara <Francinara@LAPTOP-TOP>
2019-11-18 15:21:11 -0200

    Saida e entrada de estqoue

--- src/br/com/fbd/acad/controller/FazerVendaController.java
+++ src/br/com/fbd/acad/controller/FazerVendaController.java
@@ -1,5 +1,63 @@
 package br.com.fbd.acad.controller;
 
-public class FazerVendaController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXButton;
+
+import br.com.fbd.acad.app.App;
+import br.com.fbd.acad.entidade.Funcionario;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class FazerVendaController implements Initializable{
+	
+	private Funcionario funcionario;
+	
+    @FXML private JFXButton closeButton;
+    @FXML private JFXButton salvarButton;
+    @FXML private JFXButton cancelarButton;
+    @FXML private ComboBox<?> clienteField;
+    @FXML private Label funcionarioLabel;
+    @FXML private Label valorLabel;
+    @FXML private DatePicker dataField;
+    
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		App.addOnChangeScreenListener(new App.OnChangeScreen() {
+
+			@Override
+			public void onScreenChanged(String newScreen, Object userData) {
+				//				if(Funcionario.class == userData.getClass()) {
+				funcionario =  (Funcionario)userData;
+				if(newScreen.equals("index")) {
+					funcionarioLabel.setText(funcionario.getNome());
+					if(funcionario.getId_cargo() == 0) {
+
+						//						}
+					}
+				}
+
+			}
+		});
+		closeButton.setOnMouseClicked((MouseEvent event)->{
+			App.changeScreen("index", funcionario);
+		});
+		
+		cancelarButton.setOnMouseClicked((MouseEvent event)->{
+			App.changeScreen("index", funcionario);
+		});
+		
+	}
+    
+    @FXML
+    void handleButtonAction(ActionEvent event) {
+
+    }
 
 }
